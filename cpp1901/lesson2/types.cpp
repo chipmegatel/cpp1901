c2e30605720180d7c907261c9cd17276674c3615
chipmegatel <chipmegatel@gmail.com>
2019-10-06 10:07:35 +0000

    first commit

---
+++ cpp1901/lesson2/types.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int  number1=4;
+        int  number2=5;
+        cout << number1 << "+"  << number2 << "=" << number1+number2 << endl;
+
+        return 0;
+}
