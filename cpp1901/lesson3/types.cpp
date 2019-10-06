909b0177fe9da0b0ffb399217e1d03f12dda3baf
chipmegatel <chipmegatel@gmail.com>
2019-10-06 11:16:16 +0000

    add lesson3

---
+++ cpp1901/lesson3/types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // int x=3;
+    // int y=5;
+    // cout << x+y;
+    string name1 = "first ";
+    string name2 = "second ";
+
+    // cout << name1 << "+" << name2 << endl;
+    cout << name1 << name2 << endl;
+
+
+    bool t = 0; // t true
+    bool f = 0; // f false
+    cin >> t;
+    cin >> f;
+    cout << std::boolalpha << t << f << endl;
+
+    return 0;
+}
